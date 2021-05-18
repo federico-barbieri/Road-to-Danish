790965cfe2f700bbf93227f1267be077b777d252
Federico Barbieri <federicobarbieri54@gmail.com>
2021-05-18 15:40:14 +0200

    new stuff

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
 
+
 // object with learnt words
 
 const learntWords = {
@@ -13,39 +14,101 @@ const learntWords = {
   face: "ansigt",
   cup: "kop",
   coffee: "kaffe",
-  milk: "mælk",
+  milk: "mælk"
+
 }
 
+// length of my object
+var lengthOfObject = Object.keys(learntWords).length;
 
-/* how it works
-
-if you press "practice" you'll have the word in english on one side
-and a box for the input. if the word you put matches the word in english then you get a "nice!"
-if you miss 3 times the word disappears and you have to "relearn it".
-*/
-
+//
+//
 
 // practice button
-
 const practiceBtn = document.querySelector(".practice");
-
+//
 // new word button
-
 const newWord = document.querySelector(".addNewWord");
+
+//
+//
+//
 
 // form
 const form = document.querySelector('.formu-juego');
 
-// english word input
 
-var englishWord = document.querySelector(".english-word");
+// start practicing button
+var practiceStartBtn = document.querySelector(".start-practice");
+//
+//
+// danish word input
+var danishWord = document.querySelector(".danish-word");
+//
+// english word
+var englishWord = document.querySelector(".given-english-word");
+//
+//submit your word button
+var submitBtn = document.querySelector(".submit-btn-form");
+//
+//
+// random number to get a random word from the learnt words
+function randomWord(){
+  return Math.floor(Math.random() * lengthOfObject);
+}
 
+//
+//
+//
 // event listener to practice
-
 practiceBtn.addEventListener('click', () => {
   form.style.opacity = "1";
   practiceBtn.style.opacity = "0";
   newWord.style.opacity = "0";
-  englishWord.innerHTML = Object.keys(learntWords[0]);
+})
+
+
+// counter that glues A: the position of the word in the object with B: the position of the key as a danish word
+var counter = 0;
+
+//
+
+
+// event listener after pressing "start"
+practiceStartBtn.addEventListener('click', () =>{
+
+  var randomNum = randomWord();
+  englishWord.innerHTML = "English word: " + Object.keys(learntWords)[randomNum];
+  counter = randomNum;
+})
+//
+//
+// event listener to submit your word
+
+submitBtn.addEventListener('click', (e)=> {
+  event.preventDefault();
+
+  if (danishWord.value == Object.values(learntWords)[counter]){
+
+      alert("NICEEEEE!");
+      correctWords+=1;
+    } else {
+        alert("not so nice");
+      }
+})
+
+//
+//
+// view results
+
+// view results button
+var viewResultsBtn = document.querySelector('.view-results');
+
+// counter for the amount of correct words
+var correctWords = 0;
+
+viewResultsBtn.addEventListener('click', () => {
+
+  alert("Congratulations! You had " + correctWords + " correct words!")
 
 })
